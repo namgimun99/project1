7f31841306253c35bfbe1a82f2f974085e5f03f1
남기문 <ngm011@naver.com>
2026-01-25 23:47:47 +0900

    로그인 구현

---
+++ src/main/java/com/project/project1/config/CustomUserLoginService.java
@@ -0,0 +1,34 @@
+package com.project.project1.config;
+
+import com.project.project1.domain.User;
+import com.project.project1.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+// UserDetailsService 빈 등록
+public class CustomUserLoginService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자 없음"));
+
+        return new CustomUserLogin(user);
+    }
+}
+
+
+
+
+
+
+
--- src/main/java/com/project/project1/config/SecurityConfig.java
+++ src/main/java/com/project/project1/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.project.project1.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
@@ -12,26 +14,27 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // Spring Security 5버전 이하
-//       http
-//                .csrf().disable()
-//                .authorizeHttpRequests()
-//                .requestMatchers("/public/**").permitAll()
-//                .requestMatchers("/admin/**").hasRole("ADMIN")
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-        // Spring Security 6.1 버전 이후
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/join").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(withDefaults()); // 기본 로그인 폼
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout") // 기본 로그아웃 처리 주소
+                );
+
         return http.build();
     }
 }
